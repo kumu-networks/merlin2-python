ead68e86deaf5bc651ae57a739045fecb0d1947b
Christian Hahn <christian@kumunetworks.com>
2019-03-15 15:44:08 -0700

    examples: add make_merlin2b_*.py

---
+++ examples/make_merlin2b_app.py
@@ -0,0 +1,5 @@
+from merlin2 import Merlin2bApp
+
+dut = Merlin2bApp()
+dut.init()
+#dut.setup(2, 2, 80e6, 1700e6, chain=False)
---
+++ examples/make_merlin2b_eval.py
@@ -0,0 +1,5 @@
+from merlin2 import Merlin2bEval
+
+dut = Merlin2bEval()
+dut.init()
+#dut.setup(2, 2, 80e6, 1700e6, chain=False)
