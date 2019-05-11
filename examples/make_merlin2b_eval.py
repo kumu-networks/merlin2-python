37d95c81522cff15593e29cc53cf1f92d5de7000
Christian Hahn <christian@kumunetworks.com>
2019-05-10 18:19:53 -0700

    examples: add header comment

--- examples/make_merlin2b_eval.py
+++ examples/make_merlin2b_eval.py
@@ -1,5 +1,6 @@
+"""Code snippet to create Merlin2bEval object."""
+
 from merlin2 import Merlin2bEval
 
 dut = Merlin2bEval()
 dut.init()
-#dut.setup(2, 2, 80e6, 1700e6, chain=False)
--- examples/make_merlin2b_test.py
+++ examples/make_merlin2b_test.py
@@ -1,5 +1,6 @@
+"""Code snippet to create Merlin2bTest object."""
+
 from merlin2 import Merlin2bTest
 
 dut = Merlin2bTest()
 dut.init()
-#dut.setup(2, 2, 80e6, 1700e6, chain=False)
